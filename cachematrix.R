e55f93fc1d2127dd664e37d414a1cb4bc6807ae2
SAHIL KAUSHAL <sahil.mitma@learner.manipal.edu>
2019-03-11 12:07:15 +0530

    Solution week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Special Matrix Object that can be used to cache matrix inv
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y)
+    {   x <<- y
+    inv <<- NULL        
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the matrix inverse is cached or not
+## if its not then inv is calculated.
+## This function returns inverse of matrix
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinv()
+    ## Return a matrix that is the inverse of 'x'
+    if(!is.null(inv))
+    {
+        message("Found Cached Data -  Retrieving")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinv(inv)
+    inv
 }
